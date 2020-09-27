fd2e24b35d50d070231a06812fff9bae1782fe76
Shadowgamer483 <60564162+Shadowgamer483@users.noreply.github.com>
2020-09-27 13:06:17 +0530

    Add files via upload

--- screens/search.js
+++ screens/search.js
@@ -1,14 +1,16 @@
 
 import React from 'react';
 
-import { StyleSheet, Text, TouchableHighlightBase, View ,ScrollView} from 'react-native';
+import { StyleSheet, Text, TouchableHighlightBase, View ,FlatList} from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import db from '../config';
+
 export default class Search_Screen extends React.Component{
     constructor(props){
 
-super()
+super(props)
 this.state={
-alltransactioin:[]
+alltransactioin:[],LastVisibleTransaction:null,search:""
 }
 
     }
@@ -19,12 +21,58 @@ querry.docs.map((doc)=>{
     
     this.setState({alltransactioin:[...this.state.alltransactioin,doc.data()]})
 })
+
     }
+
+fetchmoretransaction=async()=>{
+
+const querry=await db.collection("transactions").startAfter(this.state.LastVisibleTransaction).limit(10).get()
+ querry.docs.map((doc)=>{
+
+this.setState({alltransactioin:[...this.state.alltransactioin,doc.data()],LastVisibleTransaction:doc})
+
+ })
+
+}
+searchTransactions=async(text)=>{
+    var text =text.split("")
+if(text[0]==="b"){
+
+const transaction=await db.collection("transactions").where("bookID","==",text).get()
+transaction.docs.map((doc)=>{
+    this.setState({alltransactioin:[...this.state.alltransactioin,doc.data()],LastVisibleTransaction:doc})
+})
+
+}
+else if(text[0]==="s"){
+
+    const transaction=await db.collection("transactions").where("StudentID","==",text).get()
+    transaction.docs.map((doc)=>{
+        this.setState({alltransactioin:[...this.state.alltransactioin,doc.data()],LastVisibleTransaction:doc})
+    })
+
+}
+
+
+}
+
     render(){
         return(
-<ScrollView>
-{this.state.alltransactioin.map((transaction,index)=>{
-    return(
+            <View style={styles.container}>
+                <View style={styles.searchBar}>
+                    <TextInput style={styles.bar}
+                    placeholder="enterID"
+                    onChangeText={(text)=>{this.setState({search:text})}}
+                    />
+                    <TouchableOpacity onPress={()=>this.searchTransactions(this.state.search)}> </TouchableOpacity>
+                    
+                </View>
+                
+<FlatList
+
+data={this.state.alltransactioin}
+renderItem={({item})=>(
+
 <View key={index}style={{borderBottomWidth:2}}>
 
 <Text>
@@ -53,13 +101,23 @@ querry.docs.map((doc)=>{
 
 </View>
 
+)}
+keyExtractor={(item,index)=>index.toString()}
+onEndReached={this.fetchmoretransaction}
+onEndReachedThreshold={0.7}
 
-    )
-})}
 
 
-    </ScrollView>
 
+
+
+
+
+
+
+
+    />
+</View>
 
         )
 
@@ -68,7 +126,16 @@ querry.docs.map((doc)=>{
 }
 
 
-
+const styles = StyleSheet.create({ 
+    container: { flex: 1, marginTop: 20 } ,
+     searchBar:{ flexDirection:'row',
+     height:40, width:'auto', borderWidth:0.5,
+      alignItems:'center', backgroundColor:'grey',
+     }, bar:{ borderWidth:2, height:30, width:300,
+         paddingLeft:10, }, searchButton:{ borderWidth:1,
+             height:30, width:50, alignItems:'center',
+              justifyContent:'center',
+ backgroundColor:'green' } })
 
 
 
